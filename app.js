cb1d25361cb81ccf830afe823f473ae28628fc6f
ifgrdo <ifgrd.26@gmail.com>
2023-01-28 21:24:22 +0000

    weather changes

--- app.js
+++ app.js
@@ -8,19 +8,19 @@ var appid = require("./config.js").appid;
 const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.get("/", function(req, res){
+app.get("/", (req, res) => {
     res.sendFile(__dirname + "/index.html");
 })
 
-app.post("/", function(req, res){
+app.post("/", (req, res) => {
     console.log("post received");
     const query = req.body.city;
     const units = "metric";
     const url = "https://api.openweathermap.org/data/2.5/weather?q=" + query + "&appid=" + appid + "&units=" + units;
 
-    https.get(url, function(response){
+    https.get(url, (response) => {
 
-        response.on("data", function(data){
+        response.on("data", (data) => {
             const weatherData = JSON.parse(data); 
 
             const temp = weatherData.main.temp;
@@ -37,6 +37,6 @@ app.post("/", function(req, res){
     })
 })
 
-app.listen(3000, function(){
+app.listen(3000, () =>{
     console.log("server is listening on port 3000");
 })
